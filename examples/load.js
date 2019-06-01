423a74d541748749a655eeec14db730dfab2e35c
Dominic Tarr <dominic.tarr@gmail.com>
2019-06-01 20:50:52 +0200

    tidy

--- examples/load.js
+++ examples/load.js
@@ -52,32 +52,33 @@ var vectors = Vector(raf, 65536)
 var map = {}
 var C = 0
 var log = AlignedLog(process.argv[2], {block: 65536})
-  log.stream()
-  .pipe(PushSink(function (data, cb) {
-    var p = 0, p_author, p_sequence, author, sequence
-    p = bipf.seekKey(data.value, p, _value)
-    p_author = bipf.seekKey(data.value, p, _author)
-    var author = bipf.decode(data.value, p_author)
-    p_sequence = bipf.seekKey(data.value, p, _sequence)
-    var sequence = bipf.decode(data.value, p_sequence)
+
+log.stream()
+.pipe(PushSink(function (data, cb) {
+  var p = 0, p_author, p_sequence, author, sequence
+  p = bipf.seekKey(data.value, p, _value)
+  p_author = bipf.seekKey(data.value, p, _author)
+  var author = bipf.decode(data.value, p_author)
+  p_sequence = bipf.seekKey(data.value, p, _sequence)
+  var sequence = bipf.decode(data.value, p_sequence)
 //    console.log(data.seq, author)
 //    console.log(author, sequence, data.seq)
-    C++
-    if(!(C%10000)) console.log(C)
+  C++
+  if(!(C%10000)) console.log(C)
 //    return cb()
-    if(!map[author]) {
-      vectors.alloc(32, function (err, vector) {
-        async = false
-        map[author] = vector
-        vectors.set(vector, sequence, data.seq, cb)
-      })
-    }
-    else
-      vectors.set(map[author], sequence, data.seq, cb)
-  }, function () {
-    vectors.size(function (err, size) {
-      console.log(size, Object.keys(map).length, C)
+  if(!map[author]) {
+    vectors.alloc(32, function (err, vector) {
+      async = false
+      map[author] = vector
+      vectors.set(vector, sequence, data.seq, cb)
     })
-  }))
+  }
+  else
+    vectors.set(map[author], sequence, data.seq, cb)
+}, function () {
+  vectors.size(function (err, size) {
+    console.log(size, Object.keys(map).length, C)
+  })
+}))
 
 
