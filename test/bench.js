042ef434c4f956dbf12c02f438b451f42624ce3f
Dominic Tarr <dominic.tarr@gmail.com>
2019-06-06 09:50:04 +0200

    tidy

--- test/bench.js
+++ test/bench.js
@@ -8,7 +8,6 @@ var v = Vector(raf)
 
 var start = Date.now(), N = 500000
 
-//v.alloc(32, function (err, ptr) {
   var n = N, M = 100, m = M, vectors = []
   for(var i = 0; i < M; i++) (function (i) {
     v.alloc(32, function (err, ptr) {
@@ -24,39 +23,9 @@ var start = Date.now(), N = 500000
   })(i)
 
   function done (n, i, _v, ptr) {
-//    console.log('done', m, n, i, _v)
-//    v.dump(ptr, console.log)
     if(--m) return
     console.log("DONE", Date.now() - start)
   }
-
-
-  //})
-//  if(true) {
-//    for(var i = 0; i < N; i++)
-//      v.set(ptr, i, i, function () {
-//        if(--n) return
-//        console.log('done', Date.now() - start)        
-//      })
-//  }
-//  else {
-//    ;(function next (n) {
-//      if(n > N) return console.log('done', Date.now() - start)
-//      v.set(ptr, n, n, function () {
-//        setImmediate(function () {
-//          next(n+1)
-//        })
-//      })
-//    })(0)
-//  }
-//})
-
-
-process.on('exit', function () {
-  console.log('MEM USED', MEM)
-})
-
-
 
 
 
