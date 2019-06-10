0f200ac5de5b09f4d30d5a760ae628bba95481ee
Dominic Tarr <dominic.tarr@gmail.com>
2019-06-10 21:33:58 +0200

    constants needs room for length

--- constants.js
+++ constants.js
@@ -1,10 +1,11 @@
 
 module.exports = {
   block: 8,
-  vector: 16, //length, next, prev, start
-  magic: 0x12345678,
+  vector: 20, //length, next, prev, start
+  magic: 0x79a3f78e, //thanks, Math.random(),
   free: 4,
   next: 4,
   prev: 8,
   start: 12,
+  length: 16,
 }
