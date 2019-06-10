f9da0bc4d86fbdfd90622bcca500fd230863cdd1
Dominic Tarr <dominic.tarr@gmail.com>
2019-06-10 23:41:35 +0200

    test coverage for flumeview

---
+++ index.js
@@ -0,0 +1,139 @@
+var Obv         = require('obv')
+var pull        = require('pull-stream')
+var AtomicFile  = require('atomic-file')
+var HashTable   = require('./hashtable')
+var path        = require('path')
+var mkdirp      = require('mkdirp')
+var AsyncSingle = require('async-single')
+var Vectors     = require('./vector')
+var PRAF        = require('polyraf')
+/*
+the view takes a map and an array
+map[key] = [...]
+
+the array points to flume offsets.
+it may be accessed by either index or by sequence.
+(for example, streaming values greater than given sequence, forward or reverse)
+
+things in ssb this will be useful for:
+
+clock index [author : sequence] (needed for replication)
+backlink index [msg_key : reply_seq] (replies/backlinks to a message)
+general equality index [value: sequence]
+  (for any value that you want to query - would make powerful queries if you merged multiple)
+
+use intersection (and) disjunction (or), etc operators to query, just
+by looking at the index values, not loading the records.
+*/
+
+module.exports = function (version, hash, each) {
+  return function (log, name) {
+    var since = Obv()
+    var af
+    var dir = path.join(path.dirname(log.filename), name)
+    var ht
+    var vectors
+    mkdirp(dir, function (_) {
+      af = AtomicFile(path.join(dir, 'hashtable.ht'))
+      vectors = Vectors(PRAF(path.join(dir, 'vectors.vec'), {readable: true, writable: true}))
+      af.get(function (err, _value) {
+        value = _value// || {since: 0, value: {}}
+        ht = HashTable().initialize(value || 65536)
+        since.set(ht.buffer.readUInt32LE(0) - 1) //starts replication
+      })
+    })
+
+    var w = AsyncSingle(function (value, cb) {
+      if(af) {
+        if(!value)
+          vectors.destroy(function () {
+            af.destroy(cb)
+          })
+        else
+          vectors.drain(function () {
+            af.set(value,  cb)
+          })
+      }
+      else cb()
+    })
+
+    return {
+      methods: {
+        get: 'async', stream: 'source'
+      },
+      since: since,
+      //XXX TODO rewrite flume to use push-streams?
+      createSink: function (cb) {
+        return pull(
+          pull.asyncMap(function (dataseq, cb) {
+            var seq = dataseq.seq
+            var data = dataseq.value
+            var n = 0, async = false
+            each(data, seq, function (key) {
+              n++
+              if(async) throw new Error('flumeview-vector: each cannot be called async')
+              var _key = hash(key)
+              var vec = ht.get(_key)
+              if(!vec) {
+                //allocating a *new buffer* is always sync, once loaded.
+                vectors.alloc(8, function (err, vec) {
+                  ht.set(_key, vec)
+                  if(ht.get(_key) != vec) throw new Error('set failed')
+                  vectors.append(vec, seq+1, next)
+                })
+              }
+              else
+                vectors.append(vec, seq+1, function (err, _vec) {
+                  //if a new vector was allocated, update the hashtable
+                  if(_vec !== vec) ht.set(_key, _vec)
+                  next()
+                })
+
+            })
+            async = true
+            if(!n) {
+              //update sequence, even though we didn't write anything.
+              ht.buffer.writeUInt32LE(seq+1, 0)
+              w.write(ht.buffer)
+              since.set(seq)
+              cb()
+            }
+            function next () {
+              if(--n) return
+              n = -1
+              if(ht.load() > 0.5) ht.rehash()
+              ht.buffer.writeUInt32LE(seq+1, 0)
+              w.write(ht.buffer)
+              since.set(seq)
+              cb()
+            }
+          }),
+          pull.drain(null, cb)
+        )
+      },
+      get: function (opts, cb) {
+        var key = hash(opts.key), index = opts.index
+        var vec = ht.get(key)
+        if(!vec) return cb(new Error('key not found:'+opts.key))
+        vectors.get(vec, index, function (err, seq) {
+          if(err || seq === 0)
+            cb(err || new Error('not found:'+opts.key+'['+opts.index+']'))
+          log.get(seq - 1, cb)
+        })
+      },
+      stream: function (opts) {
+        var key = opts.key, index = opts.index || 0
+        if(!values[key]) return cb(new Error('key not found:'+key))
+        return function (abort, cb) {
+          if(abort) return cb(abort)
+          if(!values[key]) return cb(new Error('key not found:'+key))
+          vector.get(values[key], index++, function (err, seq) {
+            if(err) cb(err)
+            else if(seq == 0) cb(true)
+            else log.get(seq-1, cb)
+          })
+        }
+      }
+    }
+  }
+}
---
+++ test/index.js
@@ -0,0 +1,66 @@
+var tape     = require('tape')
+var Flume    = require('flumedb')
+var crypto   = require('crypto')
+var Log      = require('flumelog-aligned-offset')
+var toCompat = require('flumelog-aligned-offset/compat')
+var path     = require('path')
+var rimraf   = require('rimraf')
+
+
+var filename = '/tmp/test_flumeview-vector/log.aligned'
+rimraf.sync(path.dirname(filename))
+
+var log = toCompat(Log(filename, {block: 2*1024, codec: require('flumecodec').json}))
+
+var FlumeViewVector = require('../')
+
+function addEverything (value, seq, add, path) {
+  path = path || ''
+  for(var k in value) {
+    //simple values (but not numbers, because we'd want to range query those...)
+    if(value[k] == null || 'string' === typeof value[k] || 'boolean' === typeof value[k]) {
+      var key = path + '.' + k + ':'+value[k]
+      console.log('add:', key)
+      add(key)
+    }
+    else if('object' === typeof value[k])
+      each(value[k], seq, add, path+'.'+k)
+  }
+}
+
+function hash (s) {
+  //much more secure than necessary...
+  var h = crypto.createHash('sha256').update(s, 'utf8').digest().readUInt32LE(0)
+  console.log('hash:', s, h)
+  return h
+}
+
+var db = Flume(log)
+  .use('vec', FlumeViewVector(1, hash, addEverything))
+
+var data = [{
+  foo: true, bar: 'baz', quux: 'okay'
+},{
+  foo: false, bar: 'baz', quux: 'nope'
+},{
+  foo: 'maybe', bar: 'BAR', quux: 'okay'
+}]
+
+tape('initialize', function (t) {
+  db.append(data, function (err, offset) {
+    if(err) throw err
+    db.get(offset, function (err, value) {
+      if(err) throw err
+      console.log(value)
+      db.vec.get({key:'.bar:baz', index: 0}, function (err, value) {
+        if(err) throw err
+        t.deepEqual(value, data[0])
+        db.vec.get({key:'.bar:baz', index: 1}, function (err, value) {
+          if(err) throw err
+          t.deepEqual(value, data[1])
+          t.end()
+        })
+      })
+    })
+  })
+})
