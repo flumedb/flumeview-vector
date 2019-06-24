82e704aa02fd8a5d03513c51f0630bcf9a97506d
Dominic Tarr <dominic.tarr@gmail.com>
2019-06-24 15:02:13 +0100

    iterate the lower cursors forward

--- intersect.js
+++ intersect.js
@@ -9,40 +9,94 @@ module.exports = function (blocks, vectors, reverse, each, done) {
   var cursors = vectors.map(function (vector) {
     return new Cursor(vector, blocks.block_size, reverse)
   })
-
-  function intersect () {
+  var block, ended
+  function next () {
     var max = 0
     //will return when hits something or needs a new block
-    while(true) {
-      var matched = 0
-      while(matched < cursors.length) {
-        for(var i = 0; i < cursors.length; i++) {
-          if(!cursors[i].block) throw new Error('intersect while block unset:'+i)
-          cursor = cursors[i]
-          if(cursor.isEnded()) return done()
-
-          if(max == 0)
-            max = cursor.next()
-          else if(cursor.value > max) {
-            max = cursor.value //skip over the other items now.
-            matched = 1
-          }
-          else if(cursor.value < max)
-            cursor.next()
-          else if(cursor.value === max) matched ++
-
-          if(!cursor.block) return setImmediate(ready) //reload fields
-        }
-      }
-      each(cursors[0].value - 1)
-      //after a match, iterate everything forward
-      var b = false
+    var matched = 0
+    while(matched < cursors.length) {
       for(var i = 0; i < cursors.length; i++) {
-        cursors[i].next()
-        b = b || !cursors[i].block
+        if(!cursors[i].block) {
+          block = false
+          return 0
+        }
+
+          //throw new Error('intersect while block unset:'+i)
+        cursor = cursors[i]
+        if(cursor.isEnded()) {
+          ended = true
+          return 0 //done()
+        }
+
+        if(max == 0) {
+          max = cursor.next()
+          matched = 1
+        }
+        else if(cursor.value > max) {
+          max = cursor.value //skip over the other items now.
+          matched = 1
+        }
+        else if(cursor.value < max)
+          cursor.next()
+        else if(cursor.value === max && !cursor.matched) {
+          cursor.matched = true
+          matched ++
+        }
+
+        if(!cursor.block) return 0
       }
-      if(b) return setImmediate(ready)
     }
+    var value = cursors[0].value
+//    console.log("next", max, matched, cursors.map(function (e) { return [e.index, e.value]}))
+    //after a match, iterate everything forward
+    var b = false
+    for(var i = 0; i < cursors.length; i++) {
+      cursors[i].next()
+      cursors[i].matched = false
+      b = b || !cursors[i].block
+    }
+    block = b
+    return value
+  }
+
+  function intersect () {
+    var v
+    while(v = next()) each(v - 1)
+    if(ended) done()
+    else if(!block)
+      setImmediate(ready)
+//    var max = 0
+//    //will return when hits something or needs a new block
+//    while(true) {
+//      var matched = 0
+//      while(matched < cursors.length) {
+//        for(var i = 0; i < cursors.length; i++) {
+//          if(!cursors[i].block) throw new Error('intersect while block unset:'+i)
+//          cursor = cursors[i]
+//          if(cursor.isEnded()) return done()
+//
+//          if(max == 0)
+//            max = cursor.next()
+//          else if(cursor.value > max) {
+//            max = cursor.value //skip over the other items now.
+//            matched = 1
+//          }
+//          else if(cursor.value < max)
+//            cursor.next()
+//          else if(cursor.value === max) matched ++
+//
+//          if(!cursor.block) return setImmediate(ready) //reload fields
+//        }
+//      }
+//      each(cursors[0].value - 1)
+//      //after a match, iterate everything forward
+//      var b = false
+//      for(var i = 0; i < cursors.length; i++) {
+//        cursors[i].next()
+//        b = b || !cursors[i].block
+//      }
+//      if(b) return setImmediate(ready)
+//    }
   }
 
   function ready () {
