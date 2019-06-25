7e31af0855d3429c7f338c4826209bdf7b877d8f
Dominic Tarr <dominic.tarr@gmail.com>
2019-06-25 17:26:56 +0100

    this was broken

--- intersect.js
+++ intersect.js
@@ -5,8 +5,8 @@ var Cursor = require('./cursor')
 var CursorStream = require('./stream')
 
 function Intersect (blocks, vectors, reverse) {
-  if(vectors.length === 1)
-    return new Cursor(blocks, vectors[0], reverse)
+//  if(vectors.length === 1)
+//    return new Cursor(blocks, vectors[0], reverse)
 
   this.cursors = vectors.map(function (v) {
     return new Cursor(blocks, v, reverse)
@@ -30,40 +30,52 @@ Intersect.prototype.next = function () {
   //will return when hits something or needs a new block
   var matched = 0
   while(matched < cursors.length) {
+    matched = 0
     for(var i = 0; i < cursors.length; i++) {
       if(!cursors[i].block) {
         this.block = false
         return 0
       }
+//      if(cursor.matched) continue;
 
       cursor = cursors[i]
       if(cursor.isEnded()) {
         this.ended = true
         return 0 //done()
       }
-
       if(max == 0) {
         max = cursor.next()
         matched = 1
+        //for(var k in cursors) cursors[k].mached = false
+        //cursor.matched = true
       }
       else if(cursor.value > max) {
         max = cursor.value //skip over the other items now.
+        //reset other matches
+        //for(var k in cursors) cursors[k].mached = false
+        //cursor.matched = true
         matched = 1
       }
       else if(cursor.value < max) {
         cursor.next()
         //TODO: step forward with bigger steps
-      } else if(cursor.value === max && !cursor.matched) {
-        cursor.matched = true
+      } else if(cursor.value === max) {
+        //cursor.matched = true
         matched ++
       }
 
       if(!cursor.block) return 0
     }
+//    console.log('search', matched, max, cursors.map(function (e) {
+//      return [e.index, e.value]
+//    }))
   }
   var value = cursors[0].value
   //after a match, iterate everything forward
   var b = false
+//  console.log('match', matched, max, cursors.map(function (e) {
+//    return [e.index, e.value]
+//  }))
   for(var i = 0; i < cursors.length; i++) {
     cursors[i].next()
     cursors[i].matched = false
