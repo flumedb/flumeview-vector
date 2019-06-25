ef9eaf239d446d9d80da25243a3c8e2393f2358f
Dominic Tarr <dominic.tarr@gmail.com>
2019-06-25 17:03:09 +0100

    prevent re-entrancy

--- stream.js
+++ stream.js
@@ -2,18 +2,25 @@
 function CursorStream() {
   this.sink = null
   this._blocks = null
+  this._resuming = false
 }
 
 CursorStream.prototype.resume = function () {
+  if(this._resuming) return //prevent re-entrancy
+  this._resuming = true
+
   var self = this
   if(this.sink.paused) return
   while(!this.sink.paused && (v = this.next()))
     this.sink.write(v - 1)
 
+  this._resuming = false
+
   if(this.isEnded()) this.sink.end()
   else if(!this.block)
     this.update(this.resume.bind(this))
 }
+
 
 CursorStream.prototype.pipe = function (dest) {
   this.sink = dest
