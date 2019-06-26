14f30f07f49d78a53ebef7fa8a595201e267fe6e
Dominic Tarr <dominic.tarr@gmail.com>
2019-06-26 23:35:55 +0100

    clarify test/bench

--- test/bench.js
+++ test/bench.js
@@ -20,64 +20,27 @@ var log = toCompat(Log(
 
 var FlumeViewVector = require('../')
 
-var _dog = Buffer.from('dog')
-var _fruit = Buffer.from('fruit')
-var _boolean = Buffer.from('boolean')
-var _letter = Buffer.from('letter')
-
 var _value = Buffer.from('value')
 var _content = Buffer.from('content')
 
-//var seekPathValueContent = bipf.createSeekPath(['value', 'content'])
-
 var start = Date.now()
-function addEverything (buf, seq, add, path) {
+function addEverything (buf, seq, add) {
   var p
-  //var p = seekPathValueContent(buf, 0)
   p = bipf.seekKey(buf, 0, _value)
-  if(~p)
+  if(~p) {
     p = bipf.seekKey(buf, p, _content)
-  if(~p)
-    bipf.iterate(buf, p, function (_, _value, _key) {
-//      console.log('length', bipf.getEncodedType(buf, _value) == bipf.types.string, bipf.getEncodedLength(buf, _value))
-      if(bipf.getEncodedType(buf, _value) == bipf.types.string && bipf.getEncodedLength(buf, _value) < 100) {
-        var __key = '.'+bipf.decode(buf, _key) + ':' + bipf.decode(buf, _value)
-        add(__key)
-//        console.log(__key)
-      }
-    })
+    if(~p)
+      bipf.iterate(buf, p, function (_, _value, _key) {
+        if(bipf.getEncodedType(buf, _value) == bipf.types.string && bipf.getEncodedLength(buf, _value) < 100) {
+          var __key = '.'+bipf.decode(buf, _key) + ':' + bipf.decode(buf, _value)
+          add(__key)
+        }
+      })
+  }
 }
 
 var db = Flume(log)
   .use('vec', FlumeViewVector(1, hash, addEverything))
-//  .use('count', Reduce(1, function (acc) { return acc + 1 }))
-
-/*
-var dogs = require('dog-names')
-var fruit = ['apple', 'banana', 'cherry', 'durian', 'elderberry']
-var letters = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase()
-function random () {
-  var data = {
-    boolean: Math.random() > 0.5,
-    dog: dogs.allRandom(),
-    fruit: fruit[~~(Math.random()*fruit.length)],
-    letter: letters[~~(Math.random()*letters.length)],
-  }
-  var b = Buffer.alloc(bipf.encodingLength(data))
-  bipf.encode(data, b)
-  return b
-}
-
-var N = 1000000, data = []
-;(function next (n) {
-  if(n == N) return done()
-  var value = random()
-  data.push(value)
-  db.append(value, function () {
-    next(n+1)
-  })
-})(0)
-*/
 
 var int = setInterval(function () {
   console.log(Date.now() - start, db.since.value, db.vec && db.vec.since.value, db.count && db.count.since.value)
@@ -94,28 +57,34 @@ db.vec.since(function (v) {
   console.log('sync')
   clearInterval(int)
     setImmediate(function () {
-  //    console.log("sync")
-      var k_root = '.root:%0/JiKc99TG3lbJImZJir3hu89UABOlicjs6QAPug6ow=.sha256'
-//      var k = '.channel:patchwork'
-      var k = '.type:channel'
       var C = 0, L = 0, _seq
       var start = Date.now(), ts = Date.now()
 
-        var int = db.vec.intersects({
-          keys: [
-            '.channel:solarpunk',
-//            k_root,
-            '.type:channel'
-          ],
-          values: true
-        })
+        var int = db.vec.intersects({ keys: ['.channel:solarpunk'] })
         .pipe({
-          write: function (e) {
-//            console.log(bipf.decode(e, 0))
-            C++
-          },
+          write: function (e) { C++ },
           end :function () {
-            console.log(C, Date.now() - start)
+            console.log('channel:solarpunk', C, Date.now() - start)
+            C = 0
+            var int = db.vec.intersects({ keys: ['.type:post'] })
+            .pipe({
+              write: function (e) { C++ },
+              end :function () {
+                console.log('type:post', C, Date.now() - start)
+                console.log(C, Date.now() - start)
+                C = 0
+                var int = db.vec.intersects({ keys: [
+                  '.type:post', '.channel:solarpunk'
+                ]})
+                .pipe({
+                  write: function (e) { C++ },
+                  end :function () {
+                    console.log('type:post', C, Date.now() - start)
+                    console.log(C, Date.now() - start)
+                  }
+                })
+              }
+            })
           }
         })
     })
