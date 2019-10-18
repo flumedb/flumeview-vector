74f2db4562bf9b0d88ddbc468d48bfe866006c1a
Dominic Tarr <dominic.tarr@gmail.com>
2019-10-18 22:29:27 +1300

    get dynamic queries basically working...

--- examples/dynamic.js
+++ examples/dynamic.js
@@ -1,10 +1,17 @@
 var FlumeViewVector = require('..')
 var hash = require('string-hash')
-const DEFINED = 1, STRING = 2, OBJECT = 4
 var bipf = require('bipf')
+const IS_DEFINED = 1, STRING = 2, OBJECT = 4
+
+function isEmpty (o) {
+  for(var k in o) return false
+  return true
+}
 
 function createIndexer (indexed) {
   return function addEverything (buf, seq, add) {
+    if(isEmpty(indexed)) return
+
     ;(function recurse (p, path) {
       bipf.iterate(buf, p, function (_, _value, _key) {
         var type = bipf.getEncodedType(buf, _value)
@@ -12,11 +19,11 @@ function createIndexer (indexed) {
         var index_type = indexed[__key]
         if(!index_type) return
 
-        if(index_type & DEFINED) add(__key + '!!')
+        if(index_type & IS_DEFINED) add(__key + '!!')
 
-        if(type == bipf.types.string){
-          if(bipf.getEncodedLength(buf, _value) < 100) {
-            if(index_type & STRING) add(__key + ':' + bipf.decode(buf, _value))
+        if(type === bipf.types.string) {
+          if(index_type & STRING && bipf.getEncodedLength(buf, _value) < 100) {
+            add(__key + ':' + bipf.decode(buf, _value))
           }
         }
         else if(type == bipf.types.object) {
@@ -29,7 +36,7 @@ function createIndexer (indexed) {
 
 module.exports = function () {
   return function (log, name) {
-    var indexed = {}
+    var indexed = {}, optimistic_indexed = {}
     var vectors = FlumeViewVector(1, hash, createIndexer(indexed))(log, name)
 
     var updating = false
@@ -51,42 +58,56 @@ module.exports = function () {
             recurse(q[i])
         }
         else {
-          var prefix = /^((?:\.\w+)+)[(:!)]/.exec(q)
-          prefix = prefix && prefix[1]
+          var prefix = /^((?:\.[\w-]+)+)(!|\:[\w-]+)/.exec(q)
           //check if this term is indexed, or currently being added to index
-          if(prefix && !optimistic_indexed[prefix]) {
-            toIndex[prefix] = prefix[2] == ':' ? STRING : IS_DEFINED
+          if(prefix && !optimistic_indexed[prefix[1]]) {
+            toIndex[prefix[1]] = toIndex[prefix[1]] | (prefix[2][0] == ':' ? STRING : IS_DEFINED)
             var s = ''
-            prefix[1].split(/\./).forEach(function (e) {
+            prefix[1].split(/\./).slice(1).forEach(function (e, _, a) {
               s += '.' + e
               toIndex[s] = toIndex[s] | OBJECT
             })
           }
         }
       })(q)
-
-      if(isEmpty(toIndex) == 0)
+      if(isEmpty(toIndex)) {
         return vectors.query(opts) //we don't need to index anything
+      }
       else {
         //copy new terms into optimistic_index (indexed terms, and terms being current added)
         for(var k in toIndex)
             optimistic_indexed[k] =  toIndex[k]
       }
 
-      var stream = vectors.query(opts)
-      stream.paused = true
+      var through = {
+        paused: true,
+        write: function (d) {
+          this.sink.write(d)
+        },
+        end: function (err) {
+          this.sink.end(err)
+        },
+        resume: function () {
+          this.paused = false
+          if(this.source) this.source.resume()
+        },
+        pipe: function (sink) {
+          this.sink = sink
+          if(this.source) this.resume()
+        }
+      }
+
       vectors.update(createIndexer(toIndex), function () {
         for(var k in toIndex)
           indexed[k] = indexed[k] | toIndex[k]
-        stream.resume()
+        vectors.query(opts).pipe(through)
+        if(through.sink) through.resume()
       })
-      return stream
-      //}
+      return through
     }
-    console.error(vectors)
     return {
       methods: {
-        query: 'source'
+        query: 'sync'
       },
       createSink: vectors.createSink,
       since: vectors.since,
